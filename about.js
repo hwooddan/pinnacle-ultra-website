c2909da53626cf3ead51dcb257b7e608a5129472
Dan Harwood <harwood.dan73@gmail.com>
2026-04-02 15:06:34 +0100

    corrected typo in about.js

--- about.js
+++ about.js
@@ -1,6 +1,6 @@
 const PROJECT_ID = 'wpo056ht';
 const DATASET = 'production';
-const QUERY = encodeURIComponent('*[_type == "author"][0]{name, bio, "imageUrl": image.asset->url}');
+const QUERY = encodeURIComponent('*[_type == "author"][0]{name, bio, credentials, "imageUrl": image.asset->url}');
 const URL = `https://${PROJECT_ID}.api.sanity.io/v2021-10-21/data/query/${DATASET}?query=${QUERY}`;
 
 fetch(URL)
@@ -10,5 +10,11 @@ fetch(URL)
             document.getElementById('author-name').innerText = result.name;
             document.getElementById('author-bio').innerText = result.bio;
             document.getElementById('author-image').src = result.imageUrl;
+                if (result.credentials) {
+        const credsContainer = document.getElementById('author-credentials');
+        credsContainer.innerHTML = result.credentials
+            .map(cred => `<span class="credential-tag">${cred}</span>`)
+            .join('');
+            }
         }
-    });
+    })
