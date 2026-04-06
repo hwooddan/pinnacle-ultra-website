d68abaf6928823d5508cb525808e975a4891ae6b
Dan Harwood <harwood.dan73@gmail.com>
2026-04-06 14:34:40 +0100

    cleared main branch

--- pinnacle-ultra/schemaTypes/index.ts
+++
@@ -1,7 +0,0 @@
-import post from './post'
-import home from './home'
-import author from './author'
-import reviews from './reviews'
-import racehistory from './racehistory'
-
-export const schemaTypes = [post, home, author, reviews, racehistory]
